16ffc868dff4edcc0152ddc9063b8b56b65f41ad
abdurahman-ctis <abdurahman@ug.bilkent.edu.tr>
2018-12-24 21:28:04 +0300

    welcome page

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+// messages
+var messages = ["CTIS - Web Technologies 1<br/>PROJECT<br/>Fall 2018", 
+                "BY<br/>Abdurahman Atakishiyev<br/>21701324", 
+                "<br/>Can you solve it?<br/>"];
+var i = 1;
+
+// init
 $(function(){
 
     $(".button").mouseenter(function(){
-        $(this).animate({backgroundColor: "darkgray"}, 1000);
+        $(this).css("cursor", "pointer");
     });
+
+    var timer = setInterval(function() {
+        
+        $("#info p").fadeOut(500, function() {
+            $(this).html(messages[i]).fadeIn(500);
+            i = (i+1)%3;
+        })
+
+    }, 2000);
 
 });
