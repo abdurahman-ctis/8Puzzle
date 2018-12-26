cc3af717ba27a8db7ed2b62f566cdd6b0cc8c9a1
abdurahman-ctis <abdurahman@ug.bilkent.edu.tr>
2018-12-27 00:41:04 +0300

    fixed some stuff

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ var messages = ["CTIS - Web Technologies 1<br/>PROJECT<br/>Fall 2018",
                 "BY<br/>Abdurahman Atakishiyev<br/>21701324", 
                 "<br/>Can you solve it?<br/>"];
 var i = 1;
-var selected;
 
 // init
 $(function(){
 
+
     // Welcome screen
     $(".button").mouseenter(function(){
         $(this).css("cursor", "pointer");
+    });
+
+    $(".button").hover(function(){
+        $(this).css({background: "#34495e", color: "black"});
+    }, function(){
+        $(this).css({background: "white", color: "#34495e"});
     });
 
     var timer = setInterval(function() {
@@ -33,18 +39,9 @@ $(function(){
     // Choose screen
 
     $("img").click(function(){
-        selected = $(this).attr("src");
-        console.log(selected);
         $("img").css('box-shadow', '0px 0px 0px');
         $(this).css('box-shadow', '0px 15px 15px #888');
         $("#choose > .button").fadeIn(500);
-    });
-
-    $("#choose > .button").click(function(){
-        $("#choose").animate({left: "-=1000px", opacity: 0}, 900, function(){
-            $(this).hide();
-            $("#game").show().animate({left: "0px", opacity: 1}, 1000);
-        });
     })
 
 });
