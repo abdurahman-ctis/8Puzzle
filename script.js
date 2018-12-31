37980f957a40e8dc99f72a798549444f8ad05c31
abdurahman-ctis <abdurahman@ug.bilkent.edu.tr>
2018-12-31 18:28:04 +0300

    elements are movable

--- script.js
+++ script.js
@@ -1,68 +1,115 @@
 // messages
-var messages = ["CTIS - Web Technologies 1<br/>PROJECT<br/>Fall 2018", 
-                "BY<br/>Abdurahman Atakishiyev<br/>21701324", 
-                "<br/>Can you solve it?<br/>"];
+var messages = ["CTIS - Web Technologies 1<br/>PROJECT<br/>Fall 2018",
+    "BY<br/>Abdurahman Atakishiyev<br/>21701324",
+    "<br/>Can you solve it?<br/>"];
 var i = 1;
 
+var board = [];
+
+var empty = [0, 0];
+
+var image;
+
 // init
-$(function(){
+$(function () {
 
 
     // Welcome screen
     $("#welcome").css("display", "block");
-    $(".button").mouseenter(function(){
+    $(".button").mouseenter(function () {
         $(this).css("cursor", "pointer");
     });
 
     // Button hover effect
-    $(".button").hover(function(){
-        $(this).css({background: "#34495e", color: "black"});
-    }, function(){
-        $(this).css({background: "white", color: "#34495e"});
+    $(".button").hover(function () {
+        $(this).css({ background: "#34495e", color: "black" });
+    }, function () {
+        $(this).css({ background: "white", color: "#34495e" });
     });
 
-    var timer = setInterval(function() {
-        
-        $("#info p").fadeOut(500, function() {
+    var timer = setInterval(function () {
+
+        $("#info p").fadeOut(500, function () {
             $(this).html(messages[i]).fadeIn(500);
-            i = (i+1)%3;
+            i = (i + 1) % 3;
         })
 
     }, 2000);
 
-    $("#welcome > .button").click(function(){
+    $("#welcome > .button").click(function () {
         clearInterval(timer);
-        $("#welcome").fadeOut(500, function(){
+        $("#welcome").fadeOut(500, function () {
             $("#choose").fadeIn(500);
         });
-        
+
     });
 
     // Choose screen
 
-    $("img").click(function(){
+    $("img").click(function () {
         $("img").css('box-shadow', '0px 0px 0px');
         $(this).css('box-shadow', '0px 15px 15px #888');
+        image = $(this).attr('src');
         $("#choose > .button").fadeIn(500);
     });
 
-    $("#choose > .button").click(function(){
-        $("#choose").animate({left: "-=1000px", opacity: 0}, 900, function(){
+    $("#choose > .button").click(function () {
+        $("#choose").animate({ left: "-=1000px", opacity: 0 }, 900, function () {
             $(this).hide();
-            $("#game").show().animate({left: "0px", opacity: 1}, 1000);
+            $("#game").show().animate({ left: "0px", opacity: 1 }, 1000);
             buildBoard();
         });
     });
 
-    function buildBoard(){
-        board = [];
-        for(var i = 0 ; i < 3; i++){
-            $("#game").append("<div class='piece' id='0"+i+"'style='"+"background:url(img/1.jpg) no-repeat -0px -"+(i*150)+"px'></div>")
-            $("#game").append("<div class='piece' id='1"+i+"'style='"+"background:url(img/1.jpg) no-repeat -150px -"+(i*150)+"px'></div>")
-            $("#game").append("<div class='piece' id='2"+i+"'style='"+"background:url(img/1.jpg) no-repeat -300px -"+(i*150)+"px'></div>")
+    $("#game").hover(function () {
+        var mov = getMovables();
+        $(".piece").each(function () {
+            if (!mov.includes($(this).attr("id")))
+                $(this).css("opacity", "0.2");
+        });
+    }, function () {
+        $(".piece").each(function () {
+            $(this).css("opacity", "1");
+        });
+    });
+
+    function getMovables() {
+        var mov = [];
+        $(".piece").each(function () {
+            var id = $(this).attr('id');
+            var coor = [Number(id[0]), Number(id[1])];
+            if (Math.abs(empty[0] - coor[0]) + Math.abs(empty[1] - coor[1]) === 1)
+                mov.push(id);
+        });
+        return mov;
+    }
+
+    function buildBoard() {
+        //board = [];
+        for (var i = 0; i < 3; i++) {
+            $("#game").append("<div class='piece' id='0" + i + "'style='" + "background:url(" + image + ") no-repeat -0px -" + (i * 150) + "px'></div>")
+            $("#game").append("<div class='piece' id='1" + i + "'style='" + "background:url(" + image + ") no-repeat -150px -" + (i * 150) + "px'></div>")
+            $("#game").append("<div class='piece' id='2" + i + "'style='" + "background:url(" + image + ") no-repeat -300px -" + (i * 150) + "px'></div>")
         }
-        //document.write("<div style='width:150px;height:150px;background:url(img/1.jpg) no-repeat 0px 0px'></div>")
-        console.log(board);
+        $(".piece:first-child").css("background", "none");
+
+        $(".piece").click(function () {
+            var id = $(this).attr('id');
+            console.log(getMovables());
+            if (getMovables().includes(id)) {
+                var coor = [Number(id[0]), Number(id[1])];
+                if (empty[0] - coor[0] !== 0) {
+                    $(this).animate({ left: "-="+152*(coor[0]-empty[0])+"px" }, 300);
+                }
+                else if (empty[1] - coor[1] !== 0) {
+                    $(this).animate({ top: "-="+152*(coor[1]-empty[1])+"px" }, 300);
+                }
+                var temp =  $("#"+empty.join("")).attr('id');
+                $("#"+empty.join("")).attr('id', id);
+                $(this).attr('id', temp);
+                empty = coor;
+            }
+        });
     }
 
 
